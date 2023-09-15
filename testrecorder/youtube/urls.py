4c3ed988ce7c190898fc80756792a8e5a4b393af
ahmednagra <ahmednagra9@gmail.com>
2023-09-15 23:03:01 +0500

    library functionalities added

--- testrecorder/youtube/urls.py
+++ testrecorder/youtube/urls.py
@@ -4,6 +4,7 @@ from .views import (logout_view, index, create_broadcast,
                     CreateBroadcastView, TransitionBroadcastView, PlaylistItemsInsertView,
                     FetchPlaylistsView, CreatePlaylistView)
 from .views_w import UserChannelsView, DeleteVideoView, LoadVideoView, YouTubeVideoAPIView
+from .views_library import FetchlibraryPlaylists, SelectedPlaylistLoadVideo, RateVideoView
 
 urlpatterns = [
     path('', index, name='index'),
@@ -22,5 +23,8 @@ urlpatterns = [
     path('videos/api/',  LoadVideoView.as_view(), name='videos'),
     path('video/<str:broadcast_id>/',
          YouTubeVideoAPIView.as_view(), name='youtube_video'),
-
+        # methods call for library page
+    path('fetchlibraryplaylists/api/', FetchlibraryPlaylists.as_view(), name='fetchlibrary-playlists'),
+    path('videos/api/<str:playlistId>/', SelectedPlaylistLoadVideo.as_view(), name='videos_from_playlistId'),
+    path('videos/api/rate/<str:videoId>/', RateVideoView.as_view(), name='rate_video'),
 ]
--- testrecorder/youtube/views_library.py
+++ testrecorder/youtube/views_library.py
@@ -11,7 +11,8 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
 from .models import YoutubeUserCredential, ChannelsRecord
-
+from rest_framework.renderers import JSONRenderer
+from googleapiclient.errors import HttpError
 
 logger = logging.getLogger(__name__)
 
@@ -24,20 +25,15 @@ def create_user_youtube_object(request):
     print('Creating youtube object...')
     try:
         # Retrieve the YoutubeUserCredential object associated with the authenticated user
-        print('user=request.user 27' , request.user)
         youtube_user = YoutubeUserCredential.objects.get(user=request.user)
-        print('youtube_user 28:', youtube_user)
 
         # Retrieve the user's credentials associated with the YoutubeUserCredential object
         credentials_data = youtube_user.credential
-        print('credentials_data 33:', credentials_data)
         try:
             # Convert the JSON string to a dictionary
             credentials_data_dict = json.loads(credentials_data)
-            print('credentials_data_dict 37:', credentials_data_dict)
             # Create credentials from the dictionary
             credentials = Credentials.from_authorized_user_info(info=credentials_data_dict)
-            print('credentials 40:', credentials)
             # print('Credentials: xxxxxxxxxxx')
         except Exception as e:
             credentials = Credentials.from_authorized_user_info(info=credentials_data)
@@ -68,13 +64,12 @@ def create_user_youtube_object(request):
 
         # Create a YouTube object using the v3 version of the API and the retrieved credentials
         youtube = build('youtube', 'v3', credentials=credentials, cache_discovery=False)
-        
+
         return youtube, credentials
     except YoutubeUserCredential.DoesNotExist:
         # If the user doesn't have a YoutubeUserCredential object,
         # return an error response with 401 Unauthorized status code
         return None
-
 
 
 class UserChannelsView(APIView):
@@ -127,7 +122,7 @@ class UserChannelsView(APIView):
             if youtube is None:
                 print('youtube object creation failed!!')
                 return Response({'Error': 'Account is not a Google account'}, status=status.HTTP_401_UNAUTHORIZED)
-            
+
             print('Getting user youtube channel...')
             # Retrieve the channels associated with the user's account
             channels_response = youtube.channels().list(part='snippet', mine=True).execute()
@@ -293,14 +288,14 @@ class DeleteVideoView(APIView):
             if youtube is None:
                 print('youtube object creation failed!!')
                 return Response({'Error': 'Account is not a Google account'}, status=status.HTTP_401_UNAUTHORIZED)
-            
 
             video_id = request.data.get('video_id')
 
             # Delete the video using the video ID
             # If successful, this method returns an HTTP 204 response code (No Content).
             response = youtube.videos().delete(id=video_id).execute()
-            return Response({'message': "Video deleted successfully", 'response': response}, status=status.HTTP_204_NO_CONTENT)
+            return Response({'message': "Video deleted successfully", 'response': response},
+                            status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
             return Response({'Error': str(e)})
 
@@ -338,7 +333,7 @@ class LoadVideoView(APIView):
             if youtube is None:
                 print('youtube object creation failed!!')
                 return Response({'Error': 'Account is not a Google account'}, status=status.HTTP_401_UNAUTHORIZED)
-        
+
             # Perform the YouTube Channels API call
             channels_response = youtube.channels().list(
                 part='contentDetails',
@@ -379,7 +374,8 @@ class LoadVideoView(APIView):
                         {
                             'videoId': videoItem['snippet']['resourceId']['videoId'],
                             'videoTitle': videoItem['snippet']['title'],
-                            'videoThumbnail': videoItem['snippet']['thumbnails'].get('default', {}).get('url', 'No Thumbnail Available'),
+                            'videoThumbnail': videoItem['snippet']['thumbnails'].get('default', {}).get('url',
+                                                                                                        'No Thumbnail Available'),
                             'videoDescription': videoItem['snippet']['description'],
                         } for videoItem in playlist_videos
                         if videoItem['snippet']['title'] != 'Deleted video'
@@ -432,3 +428,197 @@ class YouTubeVideoAPIView(APIView):
             # Handle any error that occurred during the API request
             print(f'An error occurred: {e}')
             return None
+
+
+class FetchlibraryPlaylists(APIView):
+    renderer_classes = [JSONRenderer]
+
+    def get(self, request, *args, **kwargs):
+        try:
+            youtube = create_user_youtube_object(request)
+            if youtube is None:
+                raise AttributeError('youtube object creation failed!!')
+
+            # Fetch all playlists with pagination
+            fetch_playlists = True
+            playlists = []
+            page_token = ""
+            while fetch_playlists:
+                request = youtube.playlists().list(
+                    part='id, snippet,status,contentDetails',
+                    maxResults=50,
+                    mine=True,
+                    pageToken=page_token
+                )
+                response = request.execute()
+                # Get next page token
+                if "nextPageToken" in response.keys():
+                    page_token = response['nextPageToken']
+                else:
+                    fetch_playlists = False
+
+                # Add current page's playlists
+                playlists.extend(response['items'])
+
+                print('===== playlist count ===> ', len(playlists))
+
+            # Check if the playlist is empty
+            if len(playlists) == 0:
+                print("The playlist is empty.")
+                return Response({'Error': 'The playlist is empty.'}, status=status.HTTP_204_NO_CONTENT)
+            else:
+                playlist_details_list = []
+
+                # Iterating through the json list
+                for playlist in playlists:
+                    playlist_id = playlist.get('id', '')
+                    title = playlist.get('snippet', {}).get('title', '')
+                    playlist_status = playlist.get('status', {}).get('privacyStatus', '')
+                    total_videos = playlist.get('contentDetails', {}).get('itemCount', '')
+                    thumbnail = playlist.get('snippet', {}).get('thumbnails', {}).get('medium', {}).get('url', '')
+
+                    playlist_details_list.append({
+                        'playlist_id': playlist_id,
+                        'playlist_title': title,
+                        'privacy_status': playlist_status,
+                        'total_videos': total_videos,
+                        'thumbnail_url': thumbnail
+                    })
+
+            # Current channel title (assuming the first playlist belongs to the same channel)
+            channel_title = playlists[0]["snippet"]["channelTitle"]
+
+            # Dictionary with all necessary data
+            youtube_details = {
+                'channel_title': channel_title,
+                'playlists': playlist_details_list
+            }
+
+            return Response(youtube_details, status=status.HTTP_200_OK)
+
+        except Exception as err:
+            print("Error while fetching playlists: " + str(err))
+            return Response({'Error': str(err)}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class SelectedPlaylistLoadVideo(APIView):
+    """
+    API view class for loading all videos from YouTube.
+
+    Methods:
+        get(request): Load all videos
+
+     Attributes:
+        permission_classes: a list containing the IsAuthenticated permission class to ensure only authenticated users
+        can access this view.
+    """
+
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request, playlistId):
+        print('request video against this video id : 490:', playlistId)
+        """
+        Load all videos from YouTube for the specified playlistId.
+
+        Parameters:
+            request (HttpRequest): The HTTP request object.
+            playlistId (str): The ID of the playlist to load videos from.
+
+        Returns:
+            Response: A response containing the loaded videos.
+
+        Raises:
+            YoutubeUserCredential.DoesNotExist: If the authenticated user does not have a YoutubeUserCredential object.
+            Exception: If an error occurs during the loading process.
+        """
+        try:
+            youtube = create_user_youtube_object(request)
+            if youtube is None:
+                raise AttributeError('youtube object creation failed!!')
+
+            # Perform the YouTube API call to retrieve videos for the specified playlistId
+            playlist_items_response = youtube.playlistItems().list(
+                part='snippet',
+                playlistId=playlistId,
+                maxResults=50
+            ).execute()
+            # print('selected playlist detail from views  515:', playlist_items_response)
+            playlist_videos = playlist_items_response.get('items', [])
+
+            videos = []
+
+            for videoItem in playlist_videos:
+                if 'snippet' not in videoItem or 'title' not in videoItem['snippet'] or 'resourceId' not in videoItem[
+                    'snippet']:
+                    continue
+
+                # video_title = videoItem['snippet']['title']
+                video_title = videoItem.get('snippet', {}).get('title', '')  # Error handling include
+                # resource_id = videoItem['snippet']['resourceId']
+                resource_id = videoItem.get('snippet', {}).get('resourceId', {})
+                if 'videoId' not in resource_id:
+                    continue
+
+                videoId = resource_id.get('videoId', '')
+                videoTitle = video_title
+                videoThumbnail = videoItem.get('snippet', {}).get('thumbnails', {}).get('medium', {}).get('url',
+                                                                                                          'No Thumbnail Available')
+                videoDescription = videoItem.get('snippet', {}).get('description', '')
+
+                try:
+                    video_response = youtube.videos().list(
+                        # part='snippet,contentDetails,status',
+                        part='contentDetails, status',
+                        id=videoId
+                    ).execute()
+                    # print('selected video_response detail from views  543:', video_response)
+
+                    video_info = video_response.get('items', [])[0]
+                    privacyStatus = video_info.get('status', {}).get('privacyStatus', 'Unknown')
+                    duration = video_info.get('contentDetails', {}).get('duration', '00:00')
+                except HttpError as e:
+                    # Handle HttpError (e.g., video not found)
+                    privacyStatus = 'Unknown'
+                    duration = '00:00'
+
+                video_info = {
+                    'videoId': videoId,
+                    'videoTitle': videoTitle,
+                    'videoThumbnail': videoThumbnail,
+                    'videoDescription': videoDescription,
+                    'privacyStatus': privacyStatus,
+                    'duration': duration,
+                }
+                print('currently process video info 562: ', video_info)
+                videos.append(video_info)
+
+                playlist_details = {
+                    'playlist_videos': videos
+                }
+
+            return Response(playlist_details, status=status.HTTP_200_OK)
+        except Exception as e:
+            # Return an error message
+            print('videos error  >> ', e)
+            return Response({'Error': str(e)}, status=status.HTTP_404_NOT_FOUND)
+
+
+class RateVideoView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request, videoId):
+        try:
+            rating = request.data.get('rating')
+            if rating not in ['like', 'dislike']:
+                raise ValueError('Invalid rating value')
+
+            youtube = create_user_youtube_object(request)
+            if youtube is None:
+                raise AttributeError('youtube object creation failed!!')
+
+            # Perform the YouTube API call to rate the video
+            youtube.videos().rate(id=videoId, rating=rating).execute()
+
+            return Response({'message': f'Video {rating}d successfully.'}, status=status.HTTP_200_OK)
+        except Exception as e:
+            return Response({'Error': str(e)}, status=status.HTTP_400_BAD_REQUEST)
