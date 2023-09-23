181ad2cd7d981c8ce517596c69edc90b539b602b
ahmednagra <nagra50@yahoo.com>
2023-09-23 13:11:58 +0500

    Fixed Library page

--- testrecorder/youtube/views_library.py
+++ testrecorder/youtube/views_library.py
@@ -17,6 +17,64 @@ from googleapiclient.errors import HttpError
 logger = logging.getLogger(__name__)
 
 
+# def create_user_youtube_object(request):
+#     """
+#     Create a YouTube object using the v3 version of the API and
+#     the authenticated user's credentials.
+#     """
+#     print('Creating youtube object...')
+#     try:
+#         # Retrieve the YoutubeUserCredential object associated with the authenticated user
+#         print('user=request.user 27', request.user)
+#         youtube_user = YoutubeUserCredential.objects.get(user=request.user)
+#         print('youtube_user 28:', youtube_user)
+#
+#         # Retrieve the user's credentials associated with the YoutubeUserCredential object
+#         credentials_data = youtube_user.credential
+#         print('credentials_data 33:', credentials_data)
+#         try:
+#             # Convert the JSON string to a dictionary
+#             credentials_data_dict = json.loads(credentials_data)
+#             print('credentials_data_dict 37:', credentials_data_dict)
+#             # Create credentials from the dictionary
+#             credentials = Credentials.from_authorized_user_info(info=credentials_data_dict)
+#             print('credentials 40:', credentials)
+#             # print('Credentials: xxxxxxxxxxx')
+#         except Exception as e:
+#             credentials = Credentials.from_authorized_user_info(info=credentials_data)
+#             # print('Credentials: xxxxxxxxxxx')
+#         try:
+#             # print(f'Checking if access token has expired...{credentials.expired}')
+#             # Check if the access token has expired
+#             if credentials.expired:
+#                 print('Access token has expired!')
+#
+#                 print('Refreshing access token...')
+#                 import google.auth.transport.requests
+#
+#                 # Create a request object using the credentials
+#                 google_request = google.auth.transport.requests.Request()
+#
+#                 # Refresh the access token using the refresh token
+#                 credentials.refresh(google_request)
+#
+#                 # Update the stored credential data with the refreshed token
+#                 youtube_user.credential = credentials.to_json()
+#                 youtube_user.save()
+#                 print('Access token refreshed!')
+#         except Exception as e:
+#             # Handle any error that occurred while refreshing the access token
+#             print(f'An error occurred: {e}')
+#             return None
+#
+#         # Create a YouTube object using the v3 version of the API and the retrieved credentials
+#         youtube = build('youtube', 'v3', credentials=credentials, cache_discovery=False)
+#
+#         return youtube, credentials
+#     except YoutubeUserCredential.DoesNotExist:
+#         # If the user doesn't have a YoutubeUserCredential object,
+#         # return an error response with 401 Unauthorized status code
+#         return None
 def create_user_youtube_object(request):
     """
     Create a YouTube object using the v3 version of the API and
@@ -25,21 +83,24 @@ def create_user_youtube_object(request):
     print('Creating youtube object...')
     try:
         # Retrieve the YoutubeUserCredential object associated with the authenticated user
+        print('user=request.user 27', request.user)
         youtube_user = YoutubeUserCredential.objects.get(user=request.user)
+        print('youtube_user 28:', youtube_user)
 
         # Retrieve the user's credentials associated with the YoutubeUserCredential object
         credentials_data = youtube_user.credential
+        print('credentials_data 33:', credentials_data)
         try:
             # Convert the JSON string to a dictionary
             credentials_data_dict = json.loads(credentials_data)
+            print('credentials_data_dict 37:', credentials_data_dict)
             # Create credentials from the dictionary
             credentials = Credentials.from_authorized_user_info(info=credentials_data_dict)
-            # print('Credentials: xxxxxxxxxxx')
+            print('credentials 40:', credentials)
         except Exception as e:
             credentials = Credentials.from_authorized_user_info(info=credentials_data)
-            # print('Credentials: xxxxxxxxxxx')
+
         try:
-            print(f'Checking if access token has expired...{credentials.expired}')
             # Check if the access token has expired
             if credentials.expired:
                 print('Access token has expired!')
@@ -65,7 +126,8 @@ def create_user_youtube_object(request):
         # Create a YouTube object using the v3 version of the API and the retrieved credentials
         youtube = build('youtube', 'v3', credentials=credentials, cache_discovery=False)
 
-        return youtube, credentials
+        # return youtube, credentials
+        return youtube
     except YoutubeUserCredential.DoesNotExist:
         # If the user doesn't have a YoutubeUserCredential object,
         # return an error response with 401 Unauthorized status code
@@ -434,8 +496,11 @@ class FetchlibraryPlaylists(APIView):
     renderer_classes = [JSONRenderer]
 
     def get(self, request, *args, **kwargs):
+        print('started Get Method from FetchlibraryPlaylists Function ')
         try:
+
             youtube = create_user_youtube_object(request)
+            print('youtube 439:', youtube)
             if youtube is None:
                 raise AttributeError('youtube object creation failed!!')
 
@@ -451,6 +516,7 @@ class FetchlibraryPlaylists(APIView):
                     pageToken=page_token
                 )
                 response = request.execute()
+                print('response 455:', response)
                 # Get next page token
                 if "nextPageToken" in response.keys():
                     page_token = response['nextPageToken']
